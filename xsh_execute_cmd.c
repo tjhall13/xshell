1e18b7a31e5003d31d3e2254f6f7a86f79f2f9b5
Jacob Kenney <jkenney2012@gmail.com>
2014-10-27 23:06:22 -0500

    Added checking if a file is executable and did some minor refactoring. This includes my test executable file that I made, you may have to go in and chmod the file and make sure you have execution rights for it when running it Another thing to be aware of is that you will need to change char *teststr to be the full directory of where you have xshell saved on your computer

--- xsh_execute_cmd.c
+++ xsh_execute_cmd.c
@@ -5,6 +5,9 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 //linked list of paths, command, size of array, array of parameters
 int xsh_execute_cmd(struct str_llist * list, char * cmd, int argc, char ** argv){
@@ -12,32 +15,56 @@ int xsh_execute_cmd(struct str_llist * list, char * cmd, int argc, char ** argv)
 	int retval;
 	struct str_llist * ptr = NULL;
 	for(ptr = list; ptr != NULL; ptr = ptr->next){
+		struct stat sb;
 		struct dirent *pDirent;
 		DIR *pDir;
 		
 		pDir = opendir(ptr->str);
-		if(pDIr == NULL){
+		if(pDir == NULL){
 			return -1;
 		}
 		
 		while((pDirent = readdir(pDir)) != NULL){
-			//check if executable
-			if(strcmp(pDirent->d_name, cmd) == 0){
-				pid = fork();
-				if(pid == 0){
-					char *path = ptr->str; //ptr->str gives current string
-					if(path[(strlen(ptr->str) - 1)] != '/'){
-						path = strcat(path, "/");
-						path = strcat(path, cmd);
+			char *path = ptr->str;
+			if(stat(path, &sb) == 0 && sb.st_mode & S_IXUSR){
+				if(strcmp(pDirent->d_name, cmd) == 0){
+					pid = fork();
+					if(pid == 0){
+						if(path[(strlen(path) - 1)] != '/'){
+							path = strcat(path, "/");
+							path = strcat(path, cmd);
+						}else{
+							path = strcat(path, cmd);
+						}
+						execv(path, argv);
 					}else{
-						path = strcat(path, cmd);
+						waitpid(pid, &retval, 0);
 					}
-					execv(path, argv);
 				}else{
-					waitpid(pid, $retval, 0);
+					closedir (pDir);
+					break;
 				}
+			}else{
+				closedir (pDir);
+				break;
 			}
-		}
 		closedir (pDir);
+		}
 	}
-};
+	return 0;
+}
+
+int main(void){
+	char *teststr = "/mnt/hgfs/XShell";
+	struct str_llist* list = (struct str_llist*) malloc(sizeof(struct str_llist));
+	
+	list->str = teststr;
+	
+	char * cmd = "hello.sh";
+	
+	if(xsh_execute_cmd(list, cmd, 0, NULL) == 0){
+		printf("Great success!\n");
+	}
+	
+	return 0;
+}
